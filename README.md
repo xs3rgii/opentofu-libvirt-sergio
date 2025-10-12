005df59c949844d580c48e4819b8068dcca7025b
José Domingo Muñoz <josedom24@gmail.com>
2025-10-12 16:31:22 +0200

    escenarios

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # terraform-libvirt
 
+## Ejemplos
+
 * Ejemplo 1: Máquina virtual conectada a la red "default"
 * Ejemplo 2: Máquina virtual con disco adicional
 * Ejemplo 3: Máquina virtual conectada a dos redes con DHCP
 * Ejemplo 4: Máquina virtual conectada a dos redes: una con DHCP y otra con direccionamiento estático
 * Ejemplo 5: Dos máquinas virtuales conectadas entre sí
 * Ejemplo 6: Generados de escenarios con módulos
+
+## Escenarios
+
+* Escenario 1: Servidor web y cliente (máquinas debian) para prácticas de servidores web.
---
+++ escenarios/escenario1/main.tf
@@ -0,0 +1,48 @@
+module "server1" {
+  source = "./modules/vm"
+
+  name         = "servidorweb"
+  memory       = 1024
+  vcpu         = 1
+  pool_name    = var.libvirt_pool_name
+  pool_path    = var.libvirt_pool_path
+  base_image   = "debian13-base.qcow2"
+
+  networks = [
+    { network_name = "nat-dhcp", wait_for_lease = true},
+    { network_name = "muy-aislada" }
+  ]
+
+    user_data      = "${path.module}/cloud-init/server1/user-data.yaml"
+  network_config = "${path.module}/cloud-init/server1/network-config.yaml"
+
+  depends_on = [
+    libvirt_network.nat-dhcp,
+    libvirt_network.muy-aislada
+  ]
+}
+
+# Ejemplo de VM sin network_config
+module "server2" {
+  source = "./modules/vm"
+
+  name         = "cliente"
+  memory       = 1024
+  vcpu         = 1
+  pool_name    = var.libvirt_pool_name
+  pool_path    = var.libvirt_pool_path
+  base_image   = "debian13-base.qcow2"
+
+  networks = [
+    { network_name = "nat-dhcp", wait_for_lease = true},
+    { network_name = "muy-aislada" }
+  ]
+
+  user_data = "${path.module}/cloud-init/server2/user-data.yaml"
+  network_config = "${path.module}/cloud-init/server2/network-config.yaml"
+
+  depends_on = [
+    libvirt_network.nat-dhcp,
+    libvirt_network.muy-aislada
+  ]
+}
